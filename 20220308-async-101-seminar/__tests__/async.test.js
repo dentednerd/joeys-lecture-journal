22a9bbd6644cadcfb61c6467179c58195125bcfa
Joey Imlay <jo.imlay@gmail.com>
2022-03-07 11:45:53 +0000

    set up async 101 seminar

---
+++ 20220308-async-101-seminar/__tests__/async.test.js
@@ -0,0 +1,28 @@
+const { countJSFiles } = require('../async');
+
+describe('countJsFiles', () => {
+  test('invokes the callback with no error', (done) => {
+    countJSFiles(`test-data/empty-dir`, (err, jsCount) => {
+      expect(err).toBe(null);
+      done();
+    });
+  });
+  test('invokes the callback with 0 if there are no files in the directory', (done) => {
+    countJSFiles(`test-data/empty-dir`, (err, jsCount) => {
+      expect(jsCount).toBe(0);
+      done();
+    });
+  });
+  test('invokes the callback with the number of files in a folder only containing JS files', (done) => {
+    countJSFiles(`test-data/js-only-dir`, (err, jsCount) => {
+      expect(jsCount).toBe(2);
+      done();
+    });
+  });
+  test('invokes the callback with the number of files in a folder only mixed files', (done) => {
+    countJSFiles(`test-data/mixed-dir`, (err, jsCount) => {
+      expect(jsCount).toBe(3);
+      done();
+    });
+  });
+});
